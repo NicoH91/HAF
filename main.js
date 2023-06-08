d9db2d397eb7ac1ea17162deed4396533b4baa66
Nicolas Haddad <nicolasmartinhaddad@gmail.com>
2023-06-08 16:52:59 -0300

    Navbar Responsive 100% width

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function(){
-    navBar= document.querySelector(".nav-bar");
+hamburger.onclick = function () {
+    navBar = document.querySelector(".nav-bar");
     navBar.classList.toggle("active")
 }
 
 
-// Variables del formulario 
+// Variables del formulario
+// const nameInput = document.getElementById("name");
+// const email = document.getElementById("email");
+// const number = document.getElementById("number");
+// const submitForm = document.getElementById("submitForm");
+// const form = document.getElementById("form");
+// const errorElement = document.getElementById("error");
 
-const name = document.getElementById(".full-name");
-const email =document.getElementById(".email");
-const number = document.getElementById(".number")
-const submitForm = get.document.getElementById(".submitForm")
+// form.addEventListener("submit", (e) => {
+//     let messages = [];
+//     if (nameInput.value === "" || nameInput.value === null) {
+//         messages.push("Name is required");
+//     }
+//     if (messages.length > 0) {
+//         e.preventDefault();
+//         errorElement.innerText = messages.join(",");
+//     }
+// });
