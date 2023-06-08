a9bf3312fc564aeac5f24d83ae3cc83a6c5b3ca8
Nicolas Haddad <nicolasmartinhaddad@gmail.com>
2023-06-08 15:28:13 -0300

    se trabajo en el maqueteado y footer

---
+++ main.js
@@ -0,0 +1,13 @@
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function(){
+    navBar= document.querySelector(".nav-bar");
+    navBar.classList.toggle("active")
+}
+
+
+// Variables del formulario 
+
+const name = document.getElementById(".full-name");
+const email =document.getElementById(".email");
+const number = document.getElementById(".number")
+const submitForm = get.document.getElementById(".submitForm")
