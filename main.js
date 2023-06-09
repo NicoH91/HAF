241fd4278f1cad9b6f232c850c17c2829dc9ad28
Nicolas Haddad <nicolasmartinhaddad@gmail.com>
2023-06-09 17:38:24 -0300

    SVG + seccion about

--- main.js
+++ main.js
@@ -5,24 +5,6 @@ hamburger.onclick = function () {
 }
 
 
-// Variables del formulario
-// const nameInput = document.getElementById("name");
-// const email = document.getElementById("email");
-// const number = document.getElementById("number");
-// const submitForm = document.getElementById("submitForm");
-// const form = document.getElementById("form");
-// const errorElement = document.getElementById("error");
-
-// form.addEventListener("submit", (e) => {
-//     let messages = [];
-//     if (nameInput.value === "" || nameInput.value === null) {
-//         messages.push("Name is required");
-//     }
-//     if (messages.length > 0) {
-//         e.preventDefault();
-//         errorElement.innerText = messages.join(",");
-//     }
-// });
 
 
 // DOM
@@ -79,7 +61,8 @@ arrRight.addEventListener("click", () => {
     if (id > images.lenght - 1) {
         id = 0;
     }
-   slide(id);
+    slide(id);
 });
- 
+
+
 
